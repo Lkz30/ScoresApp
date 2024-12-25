b4413ea513640d20d0bfdcf4290cf7d85b108550
Luis Cano <lukaz30@gmail.com>
2024-12-25 13:37:07 +0100

    el scores viewmodel me pide una variable de tipo repository: Repository, ACA menciona que una solucion simple hubira sido agregar en el ViewModel init(repository : Repository = .init())
    pero de vedad lo encuentro confuso y prefiero declarar la variable en el viewmodel en en content view para que sea mas facil de leer, ademas creo que el codigo se hace mas entendible asi

--- ScoresApp/ContentView.swift
+++ ScoresApp/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var scoresVM = ScoresViewModel(repository: <#Repository#>)
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
