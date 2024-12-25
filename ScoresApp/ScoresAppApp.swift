33dfbc688b19d17f6d9a023276893401ce14bce2
Luis Cano <lukaz30@gmail.com>
2024-12-25 10:38:57 +0100

    Initial Commit

---
+++ ScoresApp/ScoresAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresAppApp.swift
+//  ScoresApp
+//
+//  Created by Luis Cano  on 2024-12-25.
+//
+
+import SwiftUI
+
+@main
+struct ScoresAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
