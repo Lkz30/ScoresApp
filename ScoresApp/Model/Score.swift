f8610530f750d32312df1a23e8dc952012c1cb26
Luis Cano <lukaz30@gmail.com>
2024-12-25 12:32:45 +0100

    ceacion de una struct para importar los datos json

---
+++ ScoresApp/Model/Score.swift
@@ -0,0 +1,21 @@
+//
+//  Score.swift
+//  ScoresApp
+//
+//  Created by Luis Cano  on 2024-12-25.
+//
+
+import Foundation
+
+/// Este struct es la primera parte del modelo que usare para importar y leer los datos a partir de un fichero **JSON**, el cual pertenece al protocolo **CODABLE**
+
+struct Score: Codable {
+    let id: Int
+    let title: String
+    let composer:String
+    let year: Int
+    let length: Double
+    let cover : String
+    let tracks: [String]
+}
+
